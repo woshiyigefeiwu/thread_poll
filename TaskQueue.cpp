2f800907547f7d07e78d81e15fbf54f72fc799dd
fjbq <2698043112@qq.com>
2023-04-18 15:28:42 +0800

    fjbq

---
+++ TaskQueue.cpp
@@ -0,0 +1,45 @@
+#include <queue>
+#include <mutex>
+#include "TaskQueue.h"
+using namespace std;
+
+TaskQueue::TaskQueue()
+{
+    pthread_mutex_init(&m_mutex, NULL);
+}
+
+TaskQueue::~TaskQueue()
+{
+    pthread_mutex_destroy(&m_mutex);
+}
+
+void TaskQueue::addTask(Task& task)
+{
+    pthread_mutex_lock(&m_mutex);
+    m_queue.push(task);
+    pthread_mutex_unlock(&m_mutex);
+}
+
+void TaskQueue::addTask(callback func, void* arg)
+{
+    pthread_mutex_lock(&m_mutex);
+    Task task;
+    task.function = func;
+    task.arg = arg;
+    m_queue.push(task);
+    pthread_mutex_unlock(&m_mutex);
+}
+
+Task TaskQueue::takeTask()
+{
+    Task t;
+    pthread_mutex_lock(&m_mutex);
+    if (m_queue.size() > 0)
+    {
+        t = m_queue.front();
+        m_queue.pop();
+    }
+    pthread_mutex_unlock(&m_mutex);
+    return t;
+}
+
